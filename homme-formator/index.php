b42c0e8f218e8cb3fc4fdc2ff36f3fbd5f60a530
ACHRAF369 <achraflahmam2@gmail.com>
2021-03-31 14:42:46 +0100

    creat index.php

---
+++ homme-formator/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>homme formator</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <div class="navbar-parant">
+        <div class="img-homme">
+        <img src="4229301.png" alt="">
+        </div>
+        <div class="nome-class">
+        <H4 >class1</H4>
+        </div>
+        <div>
+          
+        </div>
+
+    </div>
+
+</body>
+
+</html>
