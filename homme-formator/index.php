cec1ea2162ca6ee06a0c3dca7ac4daa73d8d8afb
ACHRAF369 <achraflahmam2@gmail.com>
2021-04-01 13:46:43 +0100

    add-dashbord-formator-login-page

--- homme-formator/index.php
+++ homme-formator/index.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>homme formator</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="styl.css">
 </head>
 
 <body>
@@ -31,7 +31,43 @@
         </div>
 
     </div>
+    <div class="student-online">
+        <div class="textSLINE">
+            <p>Student Online</p>
+        </div>
+        <div class="namestudentonline">
+            <div class="childstudent">
 
+                <input type="checkbox">
+                <label>Achraf LAHMAM | achraflahmam@gmail.com</label>
+            </div>
+            <div class="childstudent">
+
+                <input type="checkbox">
+                <label>Achraf LAHMAM | achraflahmam@gmail.com</label>
+            </div>
+            <div class="childstudent"></div>
+
+            <input type="checkbox">
+            <label>Achraf LAHMAM | achraflahmam@gmail.com</label>
+        </div>
+
+    </div>
+    <section>
+        <div class="creatbrief">
+            <input type="submit" class="creation" name="creation" value="creat briaf">
+
+        </div>
+        <div class="Pbrief">
+            <a href="C:/Users/Youcode/Desktop/Untitled.pdf" class="briefpdf"> brief 1</a>
+            <a href="C:/Users/Youcode/Desktop/Untitled.pdf" class="briefpdf"> brief 2</a>
+            <a href="C:/Users/Youcode/Desktop/Untitled.pdf" class="briefpdf"> brief 3</a>
+        </div>
+        <div class="creatbrief">
+            <input type="submit" class="creation" name="creation" value="send to all students">
+
+        </div>
+    </section>
 </body>
 
 </html>
